023445df45fa4963495d0b122a159ebd720fd554
Sarge945 <plausiblesarge34@gmail.com>
2023-05-05 02:51:30 +1000

    Changed to Version makedml format so we can have proper addons. Added No Swarm Eggs addon. Removed old $Relocators folder

--- make.cmd
+++ make.cmd
@@ -4,7 +4,8 @@
 ::Don't forget quotes when specifying paths!
 ::Use -f for feature mode, -v for version mode
 ::Pass additional parameters to add DML headers as specified in the headers folder (feature mode only for now)
-call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" "vanilla" "scp"
-@del Randomiser.7z
-7z a Randomiser.7z "%~dp0\out\*"
+call build/makedml.cmd -v "%~dp0\src" "%~dp0\out" "vanilla" "scp"
+call build/makedml.cmd -z -v "%~dp0\out" Randomiser
+::@del Randomiser.7z
+::7z a Randomiser.7z "%~dp0\out\*"
 ::pause
