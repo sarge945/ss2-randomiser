980540d23ad55c43793928f12be61866c558bc5c
Sarge945 <plausiblesarge34@gmail.com>
2023-05-05 02:51:21 +1000

    Removed headers as they are now handled by makedml

--- make.cmd
+++ make.cmd
@@ -1,7 +1,10 @@
 @echo off
+::DO NOT USE ! OR % IN YOUR FOLDER NAMES!!!!
+::IT WILL BREAK THE SCRIPT!
 ::Don't forget quotes when specifying paths!
-::Passing 1 as the fourth parameter will generate DML headers
-call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" 0
+::Use -f for feature mode, -v for version mode
+::Pass additional parameters to add DML headers as specified in the headers folder (feature mode only for now)
+call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" "vanilla" "scp" "~secmod"
 @del Randomiser.7z
 7z a Randomiser.7z "%~dp0\out\*"
 pause
