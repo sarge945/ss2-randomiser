3154fca3979384edf3a5de9be6912366be7ebb7f
Sarge945 <plausiblesarge34@gmail.com>
2023-05-05 02:51:12 +1000

    updated build script to delete release file

--- make.cmd
+++ make.cmd
@@ -2,5 +2,6 @@
 ::Don't forget quotes when specifying paths!
 ::Passing 1 as the fourth parameter will generate DML headers
 call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" 0
+@del Randomiser.7z
 7z a Randomiser.7z "%~dp0\out\*"
 pause
