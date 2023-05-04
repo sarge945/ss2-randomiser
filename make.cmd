9f8d8e77a95a42e722ca520899898976b0334f74
Sarge945 <plausiblesarge34@gmail.com>
2023-05-05 02:51:11 +1000

    Added auto 7z generation to make.cmd

--- make.cmd
+++ make.cmd
@@ -2,4 +2,5 @@
 ::Don't forget quotes when specifying paths!
 ::Passing 1 as the fourth parameter will generate DML headers
 call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" 0
+7z a Randomiser.7z "%~dp0\out\*"
 pause
