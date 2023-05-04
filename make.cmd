95297220c516a4cd7ecd457d06da289cc0a9a6f3
Sarge945 <plausiblesarge34@gmail.com>
2023-05-05 02:51:22 +1000

    Update make to no longer ignore secmod

--- make.cmd
+++ make.cmd
@@ -4,7 +4,7 @@
 ::Don't forget quotes when specifying paths!
 ::Use -f for feature mode, -v for version mode
 ::Pass additional parameters to add DML headers as specified in the headers folder (feature mode only for now)
-call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" "vanilla" "scp" "~secmod"
+call build/makedml.cmd -f "%~dp0\src" "%~dp0\out" "vanilla" "scp"
 @del Randomiser.7z
 7z a Randomiser.7z "%~dp0\out\*"
 pause
